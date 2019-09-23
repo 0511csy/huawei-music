863c89259d7c9a32450416630b69330d07fec515
chenshuyi <277376753@qq.com>
2019-09-23 22:01:24 +0800

    swiper

---
+++ src/javascript/index.js
@@ -0,0 +1,89 @@
+import './icons.js'
+import Swiper from './swiper.js'
+
+const $ = selector =>document.querySelector(selector)
+
+const $$ = selector=>document.querySelectorAll(selector)
+
+
+
+class Player{
+    constructor(node) {
+    this.root = typeof node === 'string' ? $(node) : node
+    this.songList = []
+    this.currentIndex = 0
+    this.audio = new Audio()
+    this.start()
+    this.bind()
+    //https://jirengu.github.io/data-mock/huawei-music/music-list.json
+
+  }
+    start(){
+       fetch('https://jirengu.github.io/data-mock/huawei-music/music-list.json')
+       .then(res=> 
+        res.json()
+       )
+       .then(data=>{
+           console.log(data)
+           this.songList=data
+           this.audio.src=this.songList[this.currentIndex].url
+       })
+    }
+    bind(){
+        let self =this
+        this.root.querySelector('.btn-play-pause').onclick = function() {
+            
+            // if(this.classList.contains('pause')) {
+            //     self.audio.play()
+            //     this.classList.remove('pause')
+            //     this.classList.add('playing')
+            //     this.querySelector('use').setAttribut('xlink-href','#icon-play')
+            // }else if(this.classList.contains('playing')){
+            //     self.audio.pause()
+            //     this.classList.remove('playing')
+            //     this.classList.add('pause')
+            //     this.querySelector('use').setAttribut('xlink-href','#icon-pause')
+            // }
+            if(this.classList.contains('playing')) {
+                self.audio.pause()
+                this.classList.remove('playing')
+                this.classList.add('pause')
+                this.querySelector('use').setAttribute('xlink:href','#icon-play')
+            }else if(this.classList.contains('pause')){
+                self.audio.play()
+                this.classList.remove('pause')
+                this.classList.add('playing')
+                this.querySelector('use').setAttribute('xlink:href','#icon-pause')
+            }
+        }
+        this.root.querySelector('.btn-pre').onclick= function(){
+            self.playPrevSong()
+        }
+        this.root.querySelector('.btn-next').onclick= function(){
+            self.playNextSong()
+        }
+        let swiper = new Swiper(this.root.querySelector('.panels'))
+        swiper.on('swipeLeft', function () {
+            this.classList.remove("panel1")
+            this.classList.add("panel2")
+        })
+        swiper.on('swipeRight', function () {
+            this.classList.remove("panel2")
+            this.classList.add("panel1")
+        })
+    }
+    playPrevSong() {
+        this.currentIndex = (this.songList.length+this.currentIndex-1) % this.songList.length
+        this.audio.src=this.songList[this.currentIndex].url
+        this.audio.oncanplaythrough=()=>this.audio.play()
+        }
+    playNextSong() {
+        this.currentIndex = (this.songList.length+this.currentIndex+1) % this.songList.length
+        this.audio.src=this.songList[this.currentIndex].url
+        this.audio.oncanplaythrough=()=>this.audio.play()
+    }
+}
+   
+    
+
+new Player('#player')
---
+++ src/javascript/swiper.js
@@ -0,0 +1,119 @@
+// function swipe(selector,fn){
+//     let root = selector,
+//         startx,
+//         endx,
+//         movex,
+//         clock
+    
+//     ontouchstart(e){
+//       console.log(e.target)
+//       startx= e.touches[o]
+      
+//     },
+//       ontouchmove(e){
+//         endx=e.touches[0]
+//       },
+//         if(clock){
+//           clear clock
+//           clock=setTimeout(()=>{
+//             movex=endx-startx
+//       if(movex>10){
+//         swipeLeft()
+//       }else(movex<-10){
+//         swipeRight()
+//       }
+//           },1000)
+//         }
+      
+//     return{
+//       on:on,
+//       bind:bind
+//     }
+//   }
+
+
+// let Swipe=(function(){
+//     let root = document
+//     let eventHub ={'swipeLeft':[],'swipeRight':[]}
+//     function bind(type,fn){
+//         root = node
+//     }
+//     function on(type,fn){
+//         if(eventHub[type]){
+//             eventHub[type].push(fn)
+//         }
+//     }
+//     var initX
+//     var newX
+//     var clock
+//     root.ontouchstart = function(e){
+//         initX= e.changedTouches[0].pageX
+//     }
+//     root.ontouchmove = function(e){
+//         if(clock)clearInterval(clock)
+//         clock=setTimeout(()=>{
+//             newX= e.changedTouches[0].pageX
+//             if(newX-initX>10){
+//                 eventHub['swipeRight'].forEach(fn=>fn())
+//             }else{
+//                 eventHub['swipeLeft'].forEach(fn=>fn())
+//             }
+//         },100)
+//     }
+//     return{
+//         bind:bind,
+//         on:on
+//     }
+    
+// })()
+// Swiper.bind(document.querySelector('#box'))
+// Swiper.on('swipLeft', function() {
+// console.log('swipLeft')
+// })
+// Swiper.on('swipLeft', function() {
+// console.log('swipLeft 111')
+// })
+// Swiper.on('swipRight', function() {
+// console.log('swipRight')
+// })
+// Swiper.on('swipRight', function() {
+// console.log('swipRight 222')
+// })
+
+class Swiper{
+    constructor(node){
+        if(!node) throw new Error('需要传递绑定的DOM元素')
+        let root = typeof node === 'string'?document.querySelector(node):node
+        let eventHub = {'swipeLeft':[],'swipeRight':[]}
+        let initX
+        let newX
+        let clock
+        root.ontouchstart = function(e){
+            initX= e.changedTouches[0].pageX
+        }
+        root.ontouchmove = function(e){
+            if(clock)clearInterval(clock)
+            clock=setTimeout(()=>{
+                newX= e.changedTouches[0].pageX
+                if(newX-initX>10){
+                    eventHub['swipeRight'].forEach(fn=>fn.bind(root))
+                }else{
+                    eventHub['swipeLeft'].forEach(fn=>fn.bind(root))
+                }
+            },100)
+        }
+        this.on = function(type,fn){
+            if(eventHub[type]){
+                eventHub[type].push(fn)
+            }
+        }
+        this.off = function(type,fn){
+            let index = eventHub[type].indexOf(fn)
+            if(!indx == -1){
+                eventHub[type].splice(index,1)
+            }
+        }
+    }
+}
+
+export default Swiper
